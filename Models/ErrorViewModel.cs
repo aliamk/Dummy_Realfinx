1d36f53fdeba7a31686af4f4088486ce1e9364c0
unknown <alia.azarian@gmail.com>
2020-11-12 23:53:27 +0000

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dummy_Realfinx.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
